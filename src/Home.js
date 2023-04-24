a647306d90d56636388fc902072ed995651b8421
Forum Patel <forum180301@gmail.com>
2023-04-24 10:12:43 +0530

    final commit

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import conf from "../src/assets/confg.jpg";
+import Navbar from "./components/Navbar";
 
 const Home = () => {
   const [RoomCode, setRoomCode] = useState("");
@@ -11,32 +13,54 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <form
-        action=""
-        onSubmit={submitCode}
-        className="t md:pt-12 flex flex-col items-center justify-center"
-      >
-        <div className=" flex flex-col justify-center items-center">
-          <label className="text-[30px] md:text-[40px] font-bold pt-6">
-            Enter Room Code
-          </label>
-          <input
-            type="text"
-            required
-            placeholder="Enter Room Code"
-            value={RoomCode}
-            onChange={(e) => setRoomCode(e.target.value)}
-            className="bg-blue-500 placeholder:text-white py-1.5 md:py-2 px-4 rounded-full max-w-[16rem] mt-2 text-black md:mt-6 outline-0"
-          />
+    <div className=" ">
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Hero */}
+      <div className="relative h-screen ">
+        {/* Image */}
+        <div className="absolute h-full w-full flex overflow-hidden">
+          <img src={conf} className="object-cover w-full h-full" />
         </div>
-        <button
-          type="submit"
-          className="bg-orange-500 rounded-[0.5rem] mt-4 py-1.5 px-4 font-bold"
-        >
-          Enter Room
-        </button>
-      </form>
+        {/* Overlay */}
+        <div className="absolute h-full w-full flex overflow-hidden bg-black/60"></div>
+        {/* Hero Info */}
+        <div className="lg:flex lg:pt-20 flex-col items-center justify-center relative z-10 px-6 md:max-w-[90vw] mx-auto">
+          {/* Main */}
+          <div className=" flex flex-col items-center justify-center pb-8">
+            <h1 className="text-[50px] md:text-[80px] text-white font-bold pt-12 font-serif">
+              ConnectNow
+            </h1>
+          </div>
+
+          {/* Enter Code */}
+          <form
+            onSubmit={submitCode}
+            className="text-white md:pt-12 flex flex-col items-center justify-center"
+          >
+            <div className=" flex flex-col justify-center items-center">
+              <label className="text-[30px] md:text-[40px] font-bold pt-6 font-serif">
+                Enter Room Code
+              </label>
+              <input
+                type="text"
+                required
+                placeholder="Enter Room Code"
+                value={RoomCode}
+                onChange={(e) => setRoomCode(e.target.value)}
+                className="py-1.5 md:py-2 px-4 rounded-full max-w-[14rem] mt-2 text-black md:mt-6 outline-0"
+              />
+            </div>
+            <button
+              type="submit"
+              className=" bg-blue-500 hover:bg-blue-400 duration-100 ease-out font-bold w-[5rem] md:w-[7rem] rounded-full py-[5px] md:py-[7px] mt-2 md:mt-4 font-serif"
+            >
+              Go
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
